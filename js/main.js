6e33f3289e2a0ed8926344c3b909d7d88102e9e9
Seol-Munhyeok <abc1234568@naver.com>
2025-06-10 20:13:42 +0900

    feat: 미디어 쿼리 추가

--- js/main.js
+++ js/main.js
@@ -14,3 +14,31 @@ const observer = new IntersectionObserver((entries, observer) => {
 sections.forEach((section) => {
   observer.observe(section);
 });
+
+const navToggle = document.getElementById('nav-toggle');
+const navMenu = document.getElementById('nav-menu');
+
+document.addEventListener('DOMContentLoaded', () => {
+  const navToggle = document.getElementById('nav-toggle');
+  const navMenu = document.getElementById('nav__menu');
+  const navLinks = document.querySelectorAll('.nav__link');
+  const navClose = document.getElementById('nav-close');
+
+  if (navToggle && navMenu) {
+    navToggle.addEventListener('click', () => {
+      navMenu.classList.toggle('show-menu');
+    });
+
+    navLinks.forEach((link) => {
+      link.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+      });
+    });
+  }
+
+  if (navClose && navMenu) {
+    navClose.addEventListener('click', () => {
+      navMenu.classList.remove('show-menu');
+    });
+  }
+});
